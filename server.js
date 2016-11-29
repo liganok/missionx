a3b03e72686f17008eb7ef621ae14da06df86105
liganok86 <liganok86@qq.com>
2016-11-29 18:23:12 +0800

    test

--- server.js
+++ server.js
@@ -8,10 +8,11 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var async = require('async');
 
-var swig  = require('swig');
+var swig = require('swig');
 var React = require('react');
 var ReactDOM = require('react-dom/server');
 var Router = require('react-router');
+var xml2js = require('xml2js');
 
 var config = require('./config');
 var routes = require('./app/routes');
@@ -20,29 +21,35 @@ var Mission = require('./models/mission');
 var app = express();
 
 mongoose.connect(config.database);
-mongoose.connection.on('error', function() {
+mongoose.connection.on('error', function () {
   console.info('Error: Could not connect to MongoDB. Did you forget to run `mongod`?'.red);
 });
 
 app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+/**
+ * POST /api/missions
+ * Adds new mission to the database.
+ */
+app.post('/api/missions', function (req, res, next) {
+  var misssionName = req.body.name;
+  var parser = new xml2js.Parse();
+});
 
 
-
-
-app.use(function(req, res) {
-  Router.match({ routes: routes.default, location: req.url }, function(err, redirectLocation, renderProps) {
+app.use(function (req, res) {
+  Router.match({routes: routes.default, location: req.url}, function (err, redirectLocation, renderProps) {
     if (err) {
       res.status(500).send(err.message)
     } else if (redirectLocation) {
       res.status(302).redirect(redirectLocation.pathname + redirectLocation.search)
     } else if (renderProps) {
       var html = ReactDOM.renderToString(React.createElement(Router.RoutingContext, renderProps));
-      var page = swig.renderFile('views/index.html', { html: html });
+      var page = swig.renderFile('views/index.html', {html: html});
       res.status(200).send(page);
     } else {
       res.status(404).send('Page Not Found')
@@ -50,6 +57,6 @@ app.use(function(req, res) {
   });
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
