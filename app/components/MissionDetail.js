e04828bc76e01e0fe6275f074225d5bade4324b8
Gan <liganok86@qq.com>
2017-01-20 18:07:07 +0800

    refactory

--- app/components/MissionDetail.js
+++ app/components/MissionDetail.js
@@ -3,6 +3,7 @@ import {Link} from 'react-router';
 import MissionDetailStore from '../stores/MissionDetailStore';
 import MissionDetailActions from '../actions/MissionDetailActions';
 import AddMission from './AddMission';
+import {Checkbox} from "@blueprintjs/core";
 
 class MissionDetail extends React.Component {
   constructor(props) {
@@ -34,8 +35,10 @@ class MissionDetail extends React.Component {
         <div key={mission._id} id={mission._id} className='list-group-item animated fadeIn'>
           <input type='checkbox' checked={mission.isDone}
                  onChange={MissionDetailActions.changeStatus}/>
-          <a href={"../detail/" + mission._id}><span className="H5" style={{marginLeft: 4}}>{mission.name}</span></a>
-          {mission.childNum > 0 ? <span className="badge">{mission.childNumDone}/{mission.childNum}</span>:''}
+          <a href={"../detail/" + mission._id}><span className="H5"
+                                                     style={{marginLeft: 4}}>{mission.name}</span></a>
+          {mission.childNum > 0 ?
+            <span className="badge">{mission.childNumDone}/{mission.childNum}</span> : ''}
         </div>
       );
     });
@@ -45,16 +48,16 @@ class MissionDetail extends React.Component {
         <a href={"../detail/" + this.state.parent._id}>{this.state.parent.name ? this.state.parent.name + '>' : '>'}</a>
         <div className="pull-right">
           <button type="button" className="btn-link">Edit</button>
-          <button type="button" className="btn-link" onClick={MissionDetailActions.deleteMission}>Delete</button>
+          <button type="button" className="btn-link" onClick={MissionDetailActions.deleteMission}>Delete
+          </button>
         </div>
         <div className="nav nav-list nav-divider">
           <li className="nav-divider"></li>
         </div>
-        <div className="headbox">
-          <input type="checkbox"/>
-          <span style={{marginLeft: 4}}>{this.state.mission.name}</span>
-          {/*<span className="badge pull-right">11/20</span>*/}
-          <small>{this.state.mission.name}</small>
+        <div className="pt-card pt-interactive">
+          <Checkbox lable ={this.state.mission.name}>
+            {this.state.mission.name}
+          </Checkbox>
         </div>
 
         <div>
@@ -62,9 +65,11 @@ class MissionDetail extends React.Component {
           <div className="">
             <AddMission para={{type: "TASK", parentId: this.props.params.id}}/>
             <div className="" style={{marginTop: 5}}>
-              <input type="checkbox" checked={this.state.selection.todo} onChange={MissionDetailActions.selectToDo}/>
+              <input type="checkbox" checked={this.state.selection.todo}
+                     onChange={MissionDetailActions.selectToDo}/>
               <span style={{marginRight: 5}}><small> To Do</small></span>
-              <input type="checkbox" checked={this.state.selection.done} onChange={MissionDetailActions.selectDone}/>
+              <input type="checkbox" checked={this.state.selection.done}
+                     onChange={MissionDetailActions.selectDone}/>
               <span><small> Done</small></span>
             </div>
             <div style={{marginTop: 2}}>
--- gulpfile.js
+++ gulpfile.js
@@ -13,15 +13,16 @@ var browserify = require('browserify');
 var watchify = require('watchify');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
+var minifyCSS = require('gulp-minify-css');
 
 var production = process.env.NODE_ENV === 'production';
 
 var dependencies = [
-  'alt',
-  'react',
-  'react-dom',
-  'react-router',
-  'underscore'
+    'alt',
+    'react',
+    'react-dom',
+    'react-router',
+    'underscore'
 ];
 
 /*
@@ -29,15 +30,15 @@ var dependencies = [
  | Combine all JS libraries into a single file for fewer HTTP requests.
  |--------------------------------------------------------------------------
  */
-gulp.task('vendor', function() {
-  return gulp.src([
-    'bower_components/jquery/dist/jquery.js',
-    'bower_components/bootstrap/dist/js/bootstrap.js',
-    'bower_components/magnific-popup/dist/jquery.magnific-popup.js',
-    'bower_components/toastr/toastr.js'
-  ]).pipe(concat('vendor.js'))
-    .pipe(gulpif(production, uglify({ mangle: false })))
-    .pipe(gulp.dest('public/js'));
+gulp.task('vendor', function () {
+    return gulp.src([
+        'bower_components/jquery/dist/jquery.js',
+        'bower_components/bootstrap/dist/js/bootstrap.js',
+        'bower_components/magnific-popup/dist/jquery.magnific-popup.js',
+        'bower_components/toastr/toastr.js'
+    ]).pipe(concat('vendor.js'))
+        .pipe(gulpif(production, uglify({mangle: false})))
+        .pipe(gulp.dest('public/js'));
 });
 
 /*
@@ -45,14 +46,14 @@ gulp.task('vendor', function() {
  | Compile third-party dependencies separately for faster performance.
  |--------------------------------------------------------------------------
  */
-gulp.task('browserify-vendor', function() {
-  return browserify()
-    .require(dependencies)
-    .bundle()
-    .pipe(source('vendor.bundle.js'))
-    .pipe(buffer())
-    .pipe(gulpif(production, uglify({ mangle: false })))
-    .pipe(gulp.dest('public/js'));
+gulp.task('browserify-vendor', function () {
+    return browserify()
+        .require(dependencies)
+        .bundle()
+        .pipe(source('vendor.bundle.js'))
+        .pipe(buffer())
+        .pipe(gulpif(production, uglify({mangle: false})))
+        .pipe(gulp.dest('public/js'));
 });
 
 /*
@@ -60,17 +61,17 @@ gulp.task('browserify-vendor', function() {
  | Compile only project files, excluding all third-party dependencies.
  |--------------------------------------------------------------------------
  */
-gulp.task('browserify', ['browserify-vendor'], function() {
-  return browserify({ entries: 'app/main.js', debug: true })
-    .external(dependencies)
-    .transform(babelify, { presets: ['es2015', 'react'] })
-    .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({ loadMaps: true }))
-    .pipe(gulpif(production, uglify({ mangle: false })))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('public/js'));
+gulp.task('browserify', ['browserify-vendor'], function () {
+    return browserify({entries: 'app/main.js', debug: true})
+        .external(dependencies)
+        .transform(babelify, {presets: ['es2015', 'react']})
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(gulpif(production, uglify({mangle: false})))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('public/js'));
 });
 
 /*
@@ -78,28 +79,28 @@ gulp.task('browserify', ['browserify-vendor'], function() {
  | Same as browserify task, but will also watch for changes and re-compile.
  |--------------------------------------------------------------------------
  */
-gulp.task('browserify-watch', ['browserify-vendor'], function() {
-  var bundler = watchify(browserify({ entries: 'app/main.js', debug: true }, watchify.args));
-  bundler.external(dependencies);
-  bundler.transform(babelify, { presets: ['es2015', 'react'] });
-  bundler.on('update', rebundle);
-  return rebundle();
+gulp.task('browserify-watch', ['browserify-vendor'], function () {
+    var bundler = watchify(browserify({entries: 'app/main.js', debug: true}, watchify.args));
+    bundler.external(dependencies);
+    bundler.transform(babelify, {presets: ['es2015', 'react']});
+    bundler.on('update', rebundle);
+    return rebundle();
 
-  function rebundle() {
-    var start = Date.now();
-    return bundler.bundle()
-      .on('error', function(err) {
-        gutil.log(gutil.colors.red(err.toString()));
-      })
-      .on('end', function() {
-        gutil.log(gutil.colors.green('Finished rebundling in', (Date.now() - start) + 'ms.'));
-      })
-      .pipe(source('bundle.js'))
-      .pipe(buffer())
-      .pipe(sourcemaps.init({ loadMaps: true }))
-      .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest('public/js/'));
-  }
+    function rebundle() {
+        var start = Date.now();
+        return bundler.bundle()
+            .on('error', function (err) {
+                gutil.log(gutil.colors.red(err.toString()));
+            })
+            .on('end', function () {
+                gutil.log(gutil.colors.green('Finished rebundling in', (Date.now() - start) + 'ms.'));
+            })
+            .pipe(source('bundle.js'))
+            .pipe(buffer())
+            .pipe(sourcemaps.init({loadMaps: true}))
+            .pipe(sourcemaps.write('.'))
+            .pipe(gulp.dest('public/js/'));
+    }
 });
 
 /*
@@ -108,15 +109,17 @@ gulp.task('browserify-watch', ['browserify-vendor'], function() {
  |--------------------------------------------------------------------------
  */
 gulp.task('styles', function () {
-  return gulp.src([
-    'app/stylesheets/bootstrap.css',
-    'app/stylesheets/cust.css'])
-    .pipe(concat('main.css'))
-    .pipe(gulp.dest('public/css'));
+    return gulp.src([
+        'app/stylesheets/bootstrap.css',
+        'app/stylesheets/blueprint.css',
+        'app/stylesheets/cust.css'])
+        .pipe(concat('main.css'))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('watch', function() {
-  gulp.watch('app/stylesheets/**/*.less', ['styles']);
+gulp.task('watch', function () {
+    gulp.watch('app/stylesheets/**/*.css', ['styles']);
 });
 
 gulp.task('default', ['styles', 'vendor', 'browserify-watch', 'watch']);
